50bc369b35dc872fed9eee5dfac9a89e025a4134
Spencer Steers <spencersteers@gmail.com>
2014-11-30 23:16:22 -0700

    use component

--- app/controllers/explore/index.js
+++ app/controllers/explore/index.js
@@ -1,16 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  selectedTissue: 'all',
-  seedColor: {'green': 'rgb(91,183,91)'},
-  tissueWeightNormData: function() {
-    var data  = [];
-    var st = this.selectedTissue;
-    this.get('model').forEach(function(item) {
-      data.push({'label': item.get('proteinName'),
-                 'value': item.get('averageTissueWeightNorms')[st]});
-    });
-    console.log(data);
-    return data;
-  }.property('model')
+  selectedTissue: 'all'
 });
