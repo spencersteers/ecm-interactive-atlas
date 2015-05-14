5893e97c37cf26a00018e1fb0c5cbd8d77d3b9ae
ssteers@levelsbeyond.com <Spencer Steers>
2015-05-14 02:27:50 -0600

    heroku app

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,6 @@ import ENV from 'ecm-interactive-atlas/config/environment';
 
 export default DS.RESTAdapter.extend({
   // host: 'http://127.0.0.1:8000'
-  host: window.EcmInteractiveAtlas.API_HOST
+  // host: window.EcmInteractiveAtlas.API_HOST
+  host: 'https://ecmatlas.herokuapp.com'
 });
